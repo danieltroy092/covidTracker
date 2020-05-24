f43d65fae290d8be90e1956b4a553c6a5fa5fd3b
danieltroy092 <danieltroy092@gmail.com>
2020-05-24 02:44:59 +0100

    Implemented all cards with card info

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,9 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
   try {
     const {
-      data: { confirmed, recovered, deaths, lastupdate },
+      data: { confirmed, recovered, deaths, lastUpdate },
     } = await axios.get(url);
 
-    return { confirmed, recovered, deaths, lastupdate };
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
 };
